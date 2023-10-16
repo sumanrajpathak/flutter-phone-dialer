a83c83ba8e12a71dde1a6c7bd625afd6d43fb8a7
Suman Raj Pathak <viper.suman@gmail.com>
2023-10-16 17:44:36 +0545

    :rotating_light: updated linter warning

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
               onPressed: () async {
                 FlutterPhoneDialer.dialNumber(_numberCtrl.text);
               },
-            )
+            ),
           ],
         ),
       ),
