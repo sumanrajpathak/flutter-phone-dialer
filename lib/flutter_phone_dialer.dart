e6c0596463a9e2045745ac5704d831aba1d73c0e
Suman Raj Pathak <56147503+sumanrajpathak@users.noreply.github.com>
2023-10-17 16:34:32 +0545

    :bulb: update documentation for pub score

--- lib/flutter_phone_dialer.dart
+++ lib/flutter_phone_dialer.dart
@@ -2,16 +2,23 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// Example:
+/// ```dart
+/// void main() async {
+///   await FlutterPhoneDialer.dialNumber(+441234567890);
+/// }
+///
 class FlutterPhoneDialer {
   static const MethodChannel _channel = MethodChannel('flutter_phone_dialer');
 
-// Define a static asynchronous function for making phone calls.
   static Future<bool?> dialNumber(String number) async {
-    // Use the Flutter _channel to invoke a platform-specific method.
+    /// Invokes a platform-specific method 'dialNumber' with the param 'number'.
     return await _channel.invokeMethod(
-      'dialNumber', // The name of the platform-specific method to call.
+      /// The name of the platform-specific method to call.
+      'dialNumber',
       <String, Object>{
-        'number': number, // Pass the phone number as a parameter.
+        /// Pass the phone number as a parameter.
+        'number': number,
       },
     );
   }
